24efd596ae90fc3e334480746ddb017493a4b3d4
Niel <r0758943@student.thomasmore.be>
2021-01-07 13:57:01 +0100

    basis versie edge-service, Dockerfile, git actions yaml in commentaar, lege models & controller

---
+++ src/main/java/fact/it/edgeservice/controller/GardenCenterDataController.java
@@ -0,0 +1,7 @@
+package fact.it.edgeservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GardenCenterDataController {
+}
---
+++ src/main/java/fact/it/edgeservice/model/Plant.java
@@ -0,0 +1,4 @@
+package fact.it.edgeservice.model;
+
+public class Plant {
+}
