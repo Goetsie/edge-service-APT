0b92187e29892a5e2d31debc5e40467d52870cf9
Niel <r0758943@student.thomasmore.be>
2021-01-08 22:40:12 +0100

    getMapping aangemaakt voor garden center met employees, werkt niet, postgres expert moet maar is bezien

--- stop.ps1
+++ stop.ps1
@@ -3,11 +3,15 @@ kubectl delete deployment edge-service-server-deployment
 kubectl delete deployment garden-center-service-server-deployment
 kubectl delete deployment garden-center-service-mongo-deployment
 kubectl delete deployment plant-service-server-deployment
-kubectl delete deployment plant-mongo-deployment
+kubectl delete deployment plant-service-mongo-deployment
+kubectl delete deployment employee-service-server-deployment
+kubectl delete deployment employee-service-postgres-deployment
 kubectl delete service edge-service-server
 kubectl delete service garden-center-service-server
 kubectl delete service garden-center-service-mongo
 kubectl delete service plant-service-server
 kubectl delete service plant-service-mongo
+kubectl delete service employee-service-server
+kubectl delete service employee-service-postgres
 kubectl get deployments
 kubectl get services
