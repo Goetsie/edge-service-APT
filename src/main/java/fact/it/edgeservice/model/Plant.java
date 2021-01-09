cdbbdf6573c6f37d421143a2ae3b649b21cb3fd4
lloyd <r0751957@student.thomasmore.be>
2021-01-09 14:34:45 +0100

    post + kleine fout gefixed

--- src/main/java/fact/it/edgeservice/model/Plant.java
+++ src/main/java/fact/it/edgeservice/model/Plant.java
@@ -2,28 +2,27 @@ package fact.it.edgeservice.model;
 
 
 public class Plant {
-    private String id;
+    private String plantNumber;
     private String name;
     private String description;
-    // Tuincentra? !!!!
-
+    private int gardenCenterId;
 
     public Plant() {
     }
 
-    // Change when center is needed !!!!
-    public Plant(String name, String description) {
+    public Plant(int gardenCenterId, String plantNumber, String name, String description) {
+        this.gardenCenterId = gardenCenterId;
+        this.plantNumber = plantNumber;
         this.name = name;
         this.description = description;
     }
 
-
-    public String getId() {
-        return id;
+    public String getPlantNumber() {
+        return plantNumber;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setPlantNumber(String plantNumber) {
+        this.plantNumber = plantNumber;
     }
 
     public String getName() {
@@ -40,5 +39,13 @@ public class Plant {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public int getGardenCenterId() {
+        return gardenCenterId;
+    }
+
+    public void setGardenCenterId(int gardenCenterId) {
+        this.gardenCenterId = gardenCenterId;
     }
 }
